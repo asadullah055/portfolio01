9ed385c7cb063b99cef80a0787a49404210d9b50
asadullah055 <asadullahahmed01@gmail.com>
2022-07-14 22:02:42 +0600

    about

--- js/script.js
+++ js/script.js
@@ -1,31 +1,29 @@
-let menu = document.querySelector('#menu-bars');
-let header = document.querySelector('#headers');
-let coursor1 = document.querySelector('.cursor-1')
-let coursor2 = document.querySelector('.cursor-2')
-menu.onclick = () =>{
-    menu.classList.toggle('fa-times');
-    header.classList.toggle('active');
-}
-window.onscroll = () =>{
-    menu.classList.toggle('fa-times');
-    header.classList.toggle('active');
-}
+let menu = document.querySelector("#menu-bars");
+let header = document.querySelector("#headers");
+let coursor1 = document.querySelector(".cursor-1");
+let coursor2 = document.querySelector(".cursor-2");
+menu.onclick = () => {
+  menu.classList.toggle("fa-times");
+  header.classList.toggle("active");
+};
+window.onscroll = () => {
+  menu.classList.toggle("fa-times");
+  header.classList.toggle("active");
+};
 window.onmousemove = (e) => {
-    coursor1.style.top = e.pageY + 'px'
-    coursor1.style.left = e.pageX + 'px'
-    coursor2.style.top = e.pageY + 'px'
-    coursor2.style.left = e.pageX + 'px'
-}
-document.querySelectorAll('a').forEach(links =>{
+  coursor1.style.top = e.pageY + "px";
+  coursor1.style.left = e.pageX + "px";
+  coursor2.style.top = e.pageY + "px";
+  coursor2.style.left = e.pageX + "px";
+};
+document.querySelectorAll("a").forEach((links) => {
+  links.onmouseenter = () => {
+    coursor1.classList.add("active");
+    coursor2.classList.add("active");
+  };
 
-    links.onmouseenter = () =>{
-        coursor1.classList.add('active');
-        coursor2.classList.add('active');
-    }
-
-    links.onmouseleave = () =>{
-        coursor1.classList.remove('active');
-        coursor2.classList.remove('active');
-    }
-
+  links.onmouseleave = () => {
+    coursor1.classList.remove("active");
+    coursor2.classList.remove("active");
+  };
 });
